e6ff607844cd5094c7e2b72567f9a082bd7b5791
ujjwaljha77 <jhaujjwal1970@gmail.com>
2026-05-23 12:54:29 +0530

    Day 1: Solved Two Sum and Contains Duplicate

--- DSA/Arrays/001_two_sum.cpp
+++ DSA/Arrays/001_two_sum.cpp
@@ -1,1 +1,46 @@
- 
+// Lc, 1. Two Sum
+
+// brute force approach: use two nested loops to check for all pairs of elements in the array, if we find a pair that is equal to the target, then return the indices of the two elements, else return an empty vector at the end.
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+         for(int i = 0; i < nums.size()-1; i++) {
+            for(int j = i + 1; j < nums.size(); j++){
+                if(nums[i] + nums[j] == target) {
+                    return {i,j};
+                }
+            }
+         }
+        return {};
+    }
+};
+
+// optimized approach: use an unordered_map
+// to store elements and their indices.
+// if target - current element already exists in the map,
+// return their indices.
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        unordered_map<int, int> m;
+
+        for(int i = 0; i < nums.size(); i++) {
+
+            int ans = target - nums[i];
+
+            if(m.count(ans)) {
+                return {m[ans], i};
+            }
+
+            m[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
+
+
+
--- DSA/Arrays/217_contains_duplicate.cpp
+++ DSA/Arrays/217_contains_duplicate.cpp
@@ -1,1 +1,38 @@
  
+//  LC , 217. Contains Duplicate: 
+
+//  brute force: sort the array and check for adjacent elements, if they are same then return true, else return false at the end.
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size()-1; i++) {
+            if(nums[i] == nums[i+1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+// optimized approach: use an unordered_map
+// to store frequency of elements.
+// if frequency of any element becomes greater than 1,
+// then duplicate exists.
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int>m;
+        for (int i : nums) {
+            m[i]++;
+        }
+
+        for (auto it : m) {
+            if(it.second > 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
